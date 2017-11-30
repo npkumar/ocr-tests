0c41b98db22448eef7a64f87142a07faffa3a5c6
Nitin <nitin@smove.sg>
2017-11-30 14:37:33 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const Promise = require('bluebird');
+const im = require('imagemagick');
+const Tesseract = require('tesseract.js');
+const fs = require('fs');
+const _ = require('lodash');
+var images = fs.readdirSync('./dataset');
+const data = {};
+var imageList = ['148.png', '57.jpg'];
+// var imageList =['frame10.jpg', 'frame11.jpg', 'frame12.jpg'];
+
+Tesseract.create({
+  workerPath: './node_modules/tesseract.js/dist/worker.js',
+  langPath: './eng.traineddata',
+  corePath: './node_modules/tesseract.js-core/index.js',
+});
+
+function doErodeAndDilateImage(image) {
+  return new Promise((resolve, reject) => {
+    im.convert([`./dataset/${image}`, '-resize', '320x240+0+0', '-noise', '5', '-median', '5', '-unsharp', '5', '-normalize', '-colorspace', 'Gray', `./dataset_manipulated/manipulated_${image}`], (err, stdout) => {
+      console.log(`Sharpen image ${image}...`);
+      if (err) reject(err);
+      // magick convert black.png -morphology ErodeIntensity Octagon:4 erode_intensity_black.png
+      im.convert([`./dataset_manipulated/manipulated_${image}`, '-morphology', 'ErodeIntensity', 'Octagon:2', `./dataset_eroded/eroded_${image}`], (err, stdout) => {
+        console.log(`Eroding image ${image}...`);
+        if (err) reject(err);
+        // magick convert black.png -morphology DilateIntensity Octagon:12 dilate_intensity_color.png  
+        im.convert([`./dataset_eroded/eroded_${image}`, '-morphology', 'DilateIntensity', 'Octagon:8', `./dataset_dilated/dilated_${image}`], (err, stdout) => {
+          console.log(`Dialating image ${image}...`);
+          if (err) reject(err);
+          resolve(stdout);
+        });
+      });
+    });
+  });
+}
+
+function recognizeText(image) {
+  console.log('recognizeText', image)
+  Tesseract.recognize(image, {
+    tessedit_char_whitelist: '0123456789',
+    tessedit_char_blacklist: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+  })
+    .then(result => {
+      console.log('Text', result.text);
+      console.log('Confidence', result.confidence)
+      console.log('Done!');
+      return result;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+function findText(image) {
+  return new Promise((resolve, reject) => {
+    return doErodeAndDilateImage(image)
+      .then(() => {
+        return Tesseract.recognize(`./dataset_dilated/dilated_${image}`)
+      })
+      .then(result => {
+        // console.log('Text for dilated ' + image, result.text);
+        // console.log('Confidence', result.confidence)
+        data[image].push(result.text);
+        return result;
+      })
+      .then(() => {
+        return Tesseract.recognize(`./dataset_eroded/eroded_${image}`)
+      })
+      .then(result => {
+        // console.log('Text for eroded ' + image, result.text);
+        // console.log('Confidence', result.confidence)
+        data[image].push(result.text);
+        return result;
+      })
+      .then(() => {
+        return Tesseract.recognize(`./dataset_manipulated/manipulated_${image}`)
+      })
+      .then(result => {
+        // console.log('Text for manipulated ' + image, result.text);
+        // console.log('Confidence', result.confidence)
+        data[image].push(result.text);
+        resolve(result);
+      })
+      .catch(err => reject(err));  
+  });  
+}
+
+
+// images = ['31.jpeg', '148.png'];
+Promise.mapSeries(images, image => {
+  if (!image.startsWith('.')) {
+    data[image] = [];
+    return findText(image)
+  }
+})
+.then(() => console.log(data));
